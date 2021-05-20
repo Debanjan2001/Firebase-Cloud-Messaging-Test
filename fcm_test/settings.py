d7b2fd5d08e55b3a0ccf89349037aead77e1484d
Debanjan Saha <debanjansaha2001@gmail.com>
2021-05-21 00:41:45 +0530

    Final changes

--- fcm_test/settings.py
+++ fcm_test/settings.py
@@ -168,4 +168,4 @@ FCM_DJANGO_SETTINGS = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
